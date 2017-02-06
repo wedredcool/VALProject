caba304dc99bbcb992686750c2621c70dc1b9e7b
wedredcool <wedredcool@gmail.com>
2017-02-06 20:49:36 +0300

    user import

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,18 @@ public class Calculate
 {
 	public static void main(String[] arg) 
 	{
-		System.out.println("Calculate...");
+		System.out.println("Calculate...+, -, *, /, ^");
+		Double first = Double.valueOf(arg[0]);
+		Double second = Double.valueOf(arg[1]);
+		Double summ = first + second; //+
+		Double sub = first - second; //-
+		Double mult = first * second; //*
+		Double div = first / second; // /
+		Double deg = Math.pow(first,second); //^
+		System.out.println("summ = " + summ );
+		System.out.println(" sub= " + sub );
+		System.out.println("mult = " + mult );
+		System.out.println(" div = " + div );
+		System.out.println("deg = " + deg );
 	}
 }
